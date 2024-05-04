bf1398d256922b3f7a12d79631c7994ee5f79f9f
Lê Minh Quân <marucube35@gmail.com>
2024-05-04 12:23:43 +0700

    Add SWE-158

---
+++ core/tests/swe-158/selector_clashing.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.4.0;
+
+contract SelectorClashing {
+    //ruleid: swe-158
+    function ideal_warn_timed(uint256, uint128) {}
+
+    //ruleid: swe-158
+    function link_classic_internal(uint64, int64) {}
+
+    //ruleid: swe-158
+    function available_assert_time(uint16, uint64) {}
+
+    //ruleid: swe-158
+    function branch_passphrase_public(uint256, bytes8) {}
+
+    //ruleid: swe-158
+    function passphrase_calculate_transfer(uint64, address) {}
+
+    //ruleid: swe-158
+    function many_msg_babbage(bytes1) {}
+
+    //ruleid: swe-158
+    function transfer(bytes4[9], bytes5[6], int48[11]) {}
+
+    //ruleid: swe-158
+    function gasprice_bit_ether(int128) {}
+
+    //ruleid: swe-158
+    function sign_szabo_bytecode(bytes16, uint128) {}
+
+    //ruleid: swe-158
+    function remove_good(uint256[], bytes8, bool) {}
+
+    //ruleid: swe-158
+    function pizza_mandate_apology(uint256) {}
+
+    //ruleid: swe-158
+    function coral_cable_news(uint256) {}
+
+    //ruleid: swe-158
+    function collate_propagate_storage(bytes16) {}
+}
